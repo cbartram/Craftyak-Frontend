0147001b76f130cca3f40c57af59170f15261631
cbartram <cbartram3@gmail.com>
2019-11-25 13:01:01 -0500

    Fixed bug which prevented more than 1 sku of a single product from being added to cart

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -18,22 +18,9 @@ import groupBy from 'lodash/groupBy';
 export default (state = { items: [], isFetching: false, subtotal: 0, total: 0 }, action) => {
     switch (action.type) {
         case ADD_TO_CART:
-            // First group the cart items by their name i.e { itemOne: [{...}, {...}], itemTwo: [{...}] }
-            const items = groupBy([...state.items, { ...action.payload.sku, quantity: action.payload.quantity }], 'name');
-            const updatedQuantity = [];
-
-            // For each "set" of products i.e 3 mugs, 2 cups, 9 shirts
-            Object.keys(items).forEach(key => {
-
-                // Reduce them to one object incrementing the quantity value for each item i.e 3 mugs becomes 1 object with quantity: 3
-                const reduced = items[key].reduce((prev, curr) => ({ ...curr, quantity: prev.quantity + curr.quantity }));
-
-                // Add that to a set of unique cart items with a specific quantity set
-                updatedQuantity.push(reduced)
-            });
             return {
                 ...state,
-                items: [...updatedQuantity]
+                items: [...state.items, {...action.payload.sku, quantity: action.payload.quantity }]
             };
         case REMOVE_FROM_CART:
             const data = state.items.map(item => {
